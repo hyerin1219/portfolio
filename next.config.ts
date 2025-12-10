5cf3e497f86e01e79d80fe0517dc00b6e094c121
hyerin <gpfls662@gmail.com>
2025-12-10 18:30:29 +0900

    content: 내용 수정

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     output: 'export',
+    trailingSlash: true, // 새로고침 오류
     eslint: {
-        ignoreDuringBuilds: true, // 🚨 ESLint 에러 무시하고 빌드 통과
+        ignoreDuringBuilds: true, //  ESLint 에러 무시하고 빌드 통과
     },
 };
 
